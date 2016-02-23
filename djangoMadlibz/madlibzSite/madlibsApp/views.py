c9d3b621a25cd99d1c1fa6c5b4c8c0c730083954
Ryan Fitch <ryanfitch@mac.com>
2016-02-22 16:15:03 -0800

    connecting to madlibs database

--- djangoMadlibz/madlibzSite/madlibsApp/views.py
+++ djangoMadlibz/madlibzSite/madlibsApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
 
 def index(request):
     return render(request, 'madlibsApp/home.html')
+
+def profile(request):
+    req = requests.get("http://madlibz.herokuapp.com/api/random")
+    data = req.json()
+    title = data['title']
+    return HttpResponse(title)
+
+
